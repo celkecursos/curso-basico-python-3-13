0984f3eaa37001ffd7dac9de49866ce6f3366dcb
Cesar <cesar@celke.com.br>
2024-10-21 12:56:13 -0300

    #2 - Como criar variáveis como Python

---
+++ aula02/app.py
@@ -0,0 +1,32 @@
+nome_cliente = "Cesar"      # String
+print("Nome:", nome_cliente)
+
+idade_cliente = 18          # Inteiro
+print("Idade:", idade_cliente)
+
+altura_cliente = 1.81       # Float
+print("Altura:", altura_cliente)
+
+situacao = True             # Booleano
+print("Situação:", situacao)
+situacao = False
+print("Situação:", situacao)
+
+numeros = [2, 3, 5, 7, 11]  # Lista
+print("Números:", numeros)
+print("Numero:", numeros[2])
+
+usuario = {                 # Dicionário
+    "nome": "Cesar",
+    "idade": 18,
+    "cidade": "Curitiba"
+}
+print("Nome:", usuario["nome"])
+print("Idade:", usuario["idade"])
+print("Cidade:", usuario["cidade"])
+
+curso = ("Python para iniciante", "Cesar", 2024, 197.52) # Tupla
+print("Título:", curso[0])
+print("Autor:", curso[1])
+print("Ano:", curso[2])
+print("Preço:", curso[3])
