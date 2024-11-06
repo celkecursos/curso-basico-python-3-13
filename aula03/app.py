75bf45ad0414dd8f33b3fff1b49e111d1b9d955d
Cesar <cesar@celke.com.br>
2024-11-06 17:42:41 -0300

    #3 - Como usar operadores no Python

---
+++ aula03/app.py
@@ -0,0 +1,101 @@
+# Operadores Aritméticos
+
+# Soma
+a = 5
+b = 3
+print("Soma:", a + b) # Saída: 8
+
+# Subtração
+print("Subtração:", a - b) #Saída: 2
+
+# Multiplicação
+print("Multiplicação:", a * b) # Saída: 15
+
+# Divisão
+print("Divisão:", a / b) # Saída: 1.6666...
+
+# Divisão Inteira
+print("Divisão Inteira:", a // b) # Saída: 1
+
+# Módulo (resto da divisão)
+print("Módulo:", a % b) # Saída: 2
+
+# Exponenciação
+print("Exponenciação", a ** b) # Saída: 125
+
+
+# Operadores de Atribuição
+# Atribuição simples
+x = 5
+print("Atribuição:", x) # Saída: 5
+
+# Atribuição com adição
+x += 3 # Equivalente a x = x + 3
+print("Atribuição com adição:", x) # Saída: 8
+
+# Atribuição com subtração
+x -= 2 # Equivalente a x = x - 2
+print("Atribuição com subtração", x) # Saída: 6
+
+# Atribuição com multiplicação
+x *= 4 # Equivalente a x = x * 4
+print("Atribuição com multiplicação", x) # Saída: 24
+
+# Atribuição com divisão
+x /= 6 # Equivalente a x = x / 6
+print("Atribuição com divisão:", x) # Saída: 4.0
+
+# Operadores de Comparação
+a = 10
+b = 5
+
+# Igual a
+print("Igual a:", a == b) # Saída: False
+
+# Diferente de
+print("Diferente de:", a != b) # Saída: True
+
+# Maior que
+print("Maior que:", a > b) # Saída: True
+
+# Menor que
+print("Menor que:", a < b) # Saída: False
+
+# Maior ou igual a
+print("Maior ou igual a:", a >= b) # Saída: True
+
+# Menor ou igual a
+print("Menor ou igual a:", a <= b) # Saída: False
+
+# Operadores Lógicos
+a = True
+b = False
+
+# Operador AND
+print("AND:", a and b) # Saída: False (True e False resulta em False)
+
+# Operador OR
+print("OR:", a or b) # Saída: True (True ou False resulta em True)
+
+# Operador NOT
+print("NOT:", not a) # Saída: False (Inverte o valor booleano de a)
+
+# Operadores de Identidade
+x = [1, 2, 3]
+y = x
+z = [1, 2, 3]
+
+# Operador IS (verifica se x e y são o mesmo objeto)
+print("x é y:", x is y) # Saída: True
+
+# Operador IS NOT (verifica se x e z NÃO são o mesmo objeto)
+print("x não é z:", x is not z) # Saída: True
+
+# Operadores de Associação
+frutas = ["maça", "banana", "cereja"]
+
+# Operador IN (verifica se "banana" está em frutas)
+print("banana está em frutas:", "banana" in frutas) # Saída: True
+
+# Operador NOT IN (verifica se "uva" não está em frutas)
+print("uva não está em frutas:", "uva" not in frutas) # Saída: True
